236916c9d336209c3bdb83c7441888b41339720b
Gabriel27052 <goncalves.souza27@escola.pr.gov.br>
2025-10-07 12:24:38 -0300

    Add files via upload

---
+++ Gabriel-souza-1°D/mural.php
@@ -0,0 +1,20 @@
+<?php
+include "conexao.php";
+if(isset($_POST['cadastra'])){
+    $nome  = mysqli_real_escape_string($conexao, $_POST['nome']);
+    $email = mysqli_real_escape_string($conexao, $_POST['email']);
+    $msg   = mysqli_real_escape_string($conexao, $_POST['msg']);
+
+    $sql = "INSERT INTO usuerio (nome, email, mensagem) VALUES ('$nome', '$email', '$msg')";
+    mysqli_query($conexao, $sql) or die("Erro ao inserir dados: " . mysqli_error($conexao));
+    header("Location: mural.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<title>Pedidos</title>
+<link rel="stylesheet" href="style.css"/>
